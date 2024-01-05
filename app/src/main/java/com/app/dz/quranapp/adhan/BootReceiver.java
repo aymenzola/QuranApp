049a61b4513ff1f7e72389c2c55f62a90fe1c499
Aymen <mino0675655531@gmail.com>
2024-01-05 14:33:49 +0100

    v16

---
+++ app/src/main/java/com/app/dz/quranapp/adhan/BootReceiver.java
@@ -0,0 +1,24 @@
+package com.app.dz.quranapp.adhan;
+
+import static com.app.dz.quranapp.adhan.PrayerJobIntentService.getNextPrayerTimeDelay;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class BootReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        if (intent.getAction() != null && intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED) ||
+        intent.getAction().equals("android.intent.action.QUICKBOOT_POWERON")
+                || intent.getAction().equals("com.htc.intent.action.QUICKBOOT_POWERON")) {
+            // Schedule the job again when the device is rebooted
+            Log.e("testLog","onReceive schdule next job ");
+            PrayerJobIntentService.schedulePrayerJob(getNextPrayerTimeDelay(),context);
+        }
+    }
+
+
+}
--- app/src/main/java/com/app/dz/quranapp/adhan/PrayerJobIntentService.java
+++ app/src/main/java/com/app/dz/quranapp/adhan/PrayerJobIntentService.java
@@ -5,6 +5,7 @@ import static com.app.dz.quranapp.adhan.PrayerForegroundService.NOTIFICATION_ID;
 import static com.app.dz.quranapp.adhan.AdhanActivity.JOB_ID;
 
 import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.job.JobInfo;
 import android.app.job.JobParameters;
@@ -13,6 +14,8 @@ import android.app.job.JobService;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+import android.service.notification.StatusBarNotification;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -28,7 +31,7 @@ import java.util.Locale;
 public class PrayerJobIntentService extends JobService {
 
 
-    private long getNextPrayerTimeDelay() {
+    public static long getNextPrayerTimeDelay() {
         // Implement logic to calculate the delay until the next prayer time
         // Return the delay in milliseconds
         // This will depend on your app's requirements and how you determine prayer times
@@ -59,17 +62,17 @@ public class PrayerJobIntentService extends JobService {
 
     }
 
-    private void schedulePrayerJob(long nextPrayerTimeMillis) {
+    public static void schedulePrayerJob(long nextPrayerTimeMillis,Context context) {
         long currentTimeMillis = System.currentTimeMillis();
         long delayMillis = nextPrayerTimeMillis - currentTimeMillis;
 
         if (delayMillis > 0) {
-            ComponentName serviceComponent = new ComponentName(this, PrayerJobIntentService.class);
+            ComponentName serviceComponent = new ComponentName(context, PrayerJobIntentService.class);
             JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, serviceComponent)
                     .setMinimumLatency(delayMillis) // Set the delay until the job is scheduled
                     .setOverrideDeadline(delayMillis + 1000); // Set the maximum delay for the job
 
-            JobScheduler jobScheduler = (JobScheduler) getSystemService(Context.JOB_SCHEDULER_SERVICE);
+            JobScheduler jobScheduler = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
             jobScheduler.schedule(builder.build());
         } else {
             // The next prayer time is in the past, handle this case accordingly
@@ -86,12 +89,17 @@ public class PrayerJobIntentService extends JobService {
 
         // After handling the current prayer, schedule the next one
         long nextPrayerTimeDelayMillis = getNextPrayerTimeDelay();
+        long nowPrayerTimeDelayMillis = System.currentTimeMillis();
 
         // Show a notification when the prayer time is reached
-        NotificationUtils.showPrayerNotification(this, convertMillisToTime(nextPrayerTimeDelayMillis));
+        NotificationUtils.showPrayerNotification(this, convertMillisToTime(nowPrayerTimeDelayMillis));
+
+        // Start the foreground service
+        Log.e("testLog", " startForegroundServiceIfNotExists ");
+        startForegroundServiceIfNotExists();
 
         updateForegroundServiceNotification(nextPrayerTimeDelayMillis);
-        schedulePrayerJob(nextPrayerTimeDelayMillis);
+        schedulePrayerJob(nextPrayerTimeDelayMillis,this);
         Log.e("testLog","onStartJob");
         return false;
     }
@@ -101,4 +109,41 @@ public class PrayerJobIntentService extends JobService {
         Log.e("testLog","onStopJob");
         return false;
     }
+
+    private void startForegroundServiceIfNotExists() {
+        if (!isNotificationActive(NOTIFICATION_ID)) {
+            // Notification is not active, start the foreground service
+            Log.e("testLog", "Starting foreground service");
+
+            Intent serviceIntent = new Intent(this, PrayerForegroundService.class);
+            startService(serviceIntent);
+
+            /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                startForegroundService(new Intent(this, PrayerForegroundService.class));
+            } else {
+                startService(new Intent(this, PrayerForegroundService.class));
+            }*/
+        } else {
+            Log.e("testLog", "the notification already exists ");
+        }
+    }
+
+    private boolean isNotificationActive(int notificationId) {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (notificationManager != null) {
+            // Get the list of active notifications and check if the specified ID exists
+            StatusBarNotification[] activeNotifications = new StatusBarNotification[0];
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+                activeNotifications = notificationManager.getActiveNotifications();
+            }
+            for (StatusBarNotification notification : activeNotifications) {
+                if (notification.getId() == notificationId) {
+                    return true; // Notification with the specified ID is active
+                }
+            }
+        }
+        return false; // Notification with the specified ID is not active
+    }
+
 }
